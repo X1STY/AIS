ff711b07409f4f7b52ddcac25f0af15c782cdb82
X1STY <x1stygg@gmail.com>
2023-09-22 15:19:58 +0700

    ☢️ implement exit on ESC;

--- lab1/Controller.cs
+++ lab1/Controller.cs
@@ -11,37 +11,35 @@ namespace lab1
 
         public static void Menu()
         {
-            int option=0;
             Model model = new Model();
             View view = new View();
 
             while (true)
             {
-                Console.WriteLine("1. Get all records\n2. Get a record by it's number\n3. Add a record\n4. Delete a record\n5. Exit\n");
-                bool check = Int32.TryParse(Console.ReadLine(), out option);
-                if (!check) {Console.WriteLine("Incorrect input\n"); continue;}
+                Console.WriteLine("1. Get all records\n2. Get a record by it's number\n3. Add a record\n4. Delete a record\nESC. Exit\n");
+                ConsoleKey option = Console.ReadKey().Key;
             
                 Console.WriteLine(option);
                 switch (option)
                 {
-                    case 1:
-                        {
+                    case ConsoleKey.D1:
+                        {   
                             Console.Clear();
                             Console.WriteLine($"{view.GetData(model.People)}\n");
                             break;
                         }
-                    case 2:
+                    case ConsoleKey.D2:
                         {
                             Console.Clear();
                             Console.WriteLine($"Enter number of record you need (1-{model.People.Count})");
-                            int recordNumber = Int32.Parse(Console.ReadLine());
+                            Int32.TryParse(Console.ReadLine(), out int recordNumber);
                             try
                             {
                                 Console.WriteLine(view.GetData(model.GetSingleRecord(recordNumber)));
                             } catch (Exception e) { Console.WriteLine(e.Message); }
                             break;
                         }
-                    case 3:
+                    case ConsoleKey.D3:
                         {
                             Console.Clear();
                             string input = view.EnterNewData();
@@ -51,18 +49,18 @@ namespace lab1
                             } catch (Exception e) { Console.WriteLine(e.Message); } 
                             break;
                         }
-                    case 4:
+                    case ConsoleKey.D4:
                         {
                             Console.Clear();
                             Console.WriteLine($"Enter number of record to delete (1-{model.People.Count})\n");
-                            int recordNumber = Int32.Parse(Console.ReadLine());
+                            Int32.TryParse(Console.ReadLine(), out int recordNumber);
                             try
                             {
                                 Console.WriteLine(model.DeleteRecord(model.People, recordNumber));
                             } catch (Exception e) { Console.WriteLine(e.Message); }
                             break;
                         }
-                    case 5:
+                    case ConsoleKey.Escape:
                         {
                             Environment.Exit(0);
                             break;
@@ -70,7 +68,7 @@ namespace lab1
                     default: 
                         {
                             Console.Clear();
-                            Console.WriteLine($"There is no option with number {option}\n");
+                            Console.WriteLine($"Incorrect input {option}\n");
                             break;
                         }
                 }
