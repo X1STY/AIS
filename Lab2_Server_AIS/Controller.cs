e14f3a8fa973a177e91b0fb1ca118f0dffb63f93
X1STY <x1stygg@gmail.com>
2023-10-10 20:02:31 +0700

    rewrite server to async task

--- Lab2_Server_AIS/Controller.cs
+++ Lab2_Server_AIS/Controller.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace lab2_Server_AIS
 {
@@ -16,96 +15,96 @@ namespace lab2_Server_AIS
         private static string CLIENT_IP = cfg.Read("IP", "AIS");
 
         static UdpClient udpClient;
-        private static string RecieveMessageAsync()
+        private static async Task<string> ReceiveMessageAsync()
         {
             var remoteIP = (IPEndPoint)udpClient.Client.LocalEndPoint;
             string message = "";
 
             try
             {
-                byte[] data = udpClient.ReceiveAsync().Result.Buffer;
-                message = Encoding.Unicode.GetString(data);
+                UdpReceiveResult result = await udpClient.ReceiveAsync();
+                message = Encoding.Unicode.GetString(result.Buffer);
             }
             catch (Exception e) { Console.WriteLine(e.Message); }
             return message;
         }
-        private static void SendMessageAsync(string msg)
+        private static async Task SendMessageAsync(string msg)
         {
             try
             {
                 byte[] data = Encoding.Unicode.GetBytes(msg);
-                udpClient.SendAsync(data, data.Length, CLIENT_IP, CLIENT_PORT);
+                await udpClient.SendAsync(data, data.Length, CLIENT_IP, CLIENT_PORT);
             }
             catch (Exception e) { Console.WriteLine(e.Message); }
         }
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             try
             {
                 udpClient = new UdpClient(SERVER_PORT);
                 Console.WriteLine($"Server has been started on {SERVER_PORT} PORT");
-                Menu();
+                await Task.Run(() =>  MenuAsync());
             }
             catch (Exception e) { Console.WriteLine(e.Message); }
         }
 
-            static void Menu()
-            {
-                Model model = new Model();
-                View view = new View();
+        static async Task MenuAsync()
+        {
+            Model model = new Model();
+            View view = new View();
 
-                while (true)
-                {
-                SendMessageAsync("1. Get all records\n2. Get a record by it's number\n3. Add a record\n4. Delete a record\nESC. Exit\n");
-                string option = RecieveMessageAsync();
+            while (true)
+            {
+                await SendMessageAsync("1. Get all records\n2. Get a record by its number\n3. Add a record\n4. Delete a record\nESC. Exit\n");
+                string option = await ReceiveMessageAsync();
                 switch (option)
                 {
                     case "1":
                         {
-                            SendMessageAsync($"{view.GetData(model.People)}\n");
+                            await SendMessageAsync($"{view.GetData(model.People)}\n");
                             break;
                         }
                     case "2":
                         {
-                            SendMessageAsync($"Enter number of record you need (1-{model.People.Count})");
-                            Int32.TryParse(RecieveMessageAsync(), out int recordNumber);
+                            await SendMessageAsync($"Enter number of record you need (1-{model.People.Count})");
+                            Int32.TryParse(await ReceiveMessageAsync(), out int recordNumber);
                             try
                             {
-                                SendMessageAsync(view.GetData(model.GetSingleRecord(recordNumber)));
+                                await SendMessageAsync(view.GetData(model.GetSingleRecord(recordNumber)));
                             }
-                            catch (Exception e) { SendMessageAsync(e.Message); }
+                            catch (Exception e) { await SendMessageAsync(e.Message); }
                             break;
                         }
                     case "3":
                         {
-                            SendMessageAsync("Enter data about new object in following way:\nFirst Name,Last Name,Age,Is this person alive(true or false)");
-                            string input = RecieveMessageAsync().Replace(',', ';');
+                            await SendMessageAsync("Enter data about a new object in the following way:\nFirst Name,Last Name,Age,Is this person alive(true or false)");
+                            string input = await ReceiveMessageAsync();
                             try
                             {
-                                SendMessageAsync(model.AddRecord(model.People, input));
+                                await SendMessageAsync(model.AddRecord(model.People, input.Replace(',', ';')));
                             }
-                            catch (Exception e) { SendMessageAsync(e.Message); }
+                            catch (Exception e) { await SendMessageAsync(e.Message); }
                             break;
                         }
                     case "4":
                         {
-                            SendMessageAsync($"Enter number of record to delete (1-{model.People.Count})\n");
-                            Int32.TryParse(RecieveMessageAsync(), out int recordNumber);
+                            await SendMessageAsync($"Enter the number of the record to delete (1-{model.People.Count})\n");
+                            Int32.TryParse(await ReceiveMessageAsync(), out int recordNumber);
                             try
                             {
-                                SendMessageAsync(model.DeleteRecord(model.People, recordNumber));
+                                await SendMessageAsync(model.DeleteRecord(model.People, recordNumber));
                             }
-                            catch (Exception e) { SendMessageAsync(e.Message); }
+                            catch (Exception e) { await SendMessageAsync(e.Message); }
                             break;
                         }
                     default:
                         {
-                            SendMessageAsync($"Incorrect input {option}\n");
+                            await SendMessageAsync($"Incorrect input {option}\n");
                             break;
                         }
-                    }
                 }
             }
         }
     }
+}
